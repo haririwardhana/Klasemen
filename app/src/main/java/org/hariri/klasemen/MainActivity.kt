d695b9aa9965a001936025b359c3f13c39b557c8
Hariri Wardhana <haririwardhana@student.telkomuniversity.ac.id>
2024-02-23 15:48:03 +0700

    Base Project

---
+++ app/src/main/java/org/hariri/klasemen/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.hariri.klasemen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
